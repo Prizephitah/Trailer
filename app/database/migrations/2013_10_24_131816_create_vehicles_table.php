d3a5811f46d2f29351742923bcf708edc519590d
Björn Hjortsten <bjornhjortsten@gmail.com>
2013-10-24 14:02:19 +0200

    Added a table for vehicles.

---
+++ app/database/migrations/2013_10_24_131816_create_vehicles_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('vehicles', function($t) {
+			$t->engine = 'InnoDB';
+			
+			$t->increments('id');
+			$t->integer('group_id')->unsigned();
+			$t->string('license_plate', 255);
+			$t->string('name', 255);
+			$t->text('description');
+			$t->date('model_year');
+			$t->integer('curb_weight');
+			$t->integer('gross_weight');
+			$t->integer('length');
+			$t->integer('height');
+			$t->integer('created_by')->unsigned();
+			$t->dateTime('created');
+			$t->integer('updated_by')->nullable()->unsigned();
+			$t->dateTime('updated')->nullable();
+			
+			$t->softDeletes();
+			$t->index('created_by');
+			$t->foreign('group_id')->references('id')->on('groups');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('vehicles');
+	}
+
+}
